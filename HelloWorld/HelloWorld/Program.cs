96228a15934f12310d4eb76545650b909f0ab2fa
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-08-22 11:09:14 -0300

    Aula 01 - Hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("A Turma .NET 03 é TOP!");
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Teste.cs
@@ -0,0 +1,7 @@
+﻿
+namespace HelloWorld
+{
+    internal class Teste
+    {
+    }
+}
