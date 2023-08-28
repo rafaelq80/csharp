c152d459a508ba361a019a87f734cb584bef3661
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-08-28 17:24:16 -0300

    Aula 05

---
+++ aula_05/Exe03/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Exe03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3, 3];
+            int somaPrincipal = 0, somaSecundaria = 0;
+            string diagonalPrincipal = "", diagonalSecundaria = "";
+
+            for (int indiceLinha = 0; indiceLinha < Math.Sqrt(matriz.Length); indiceLinha++)
+            {
+                for (int indiceColuna = 0; indiceColuna < Math.Sqrt(matriz.Length); indiceColuna++)
+                {
+                    Console.Write($"matriz[{indiceLinha}, {indiceColuna}] = ");
+                    matriz[indiceLinha, indiceColuna] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int indice = 0; indice < Math.Sqrt(matriz.Length); indice++)
+            {
+                diagonalPrincipal += matriz[indice, indice] + " ";
+                somaPrincipal += matriz[indice, indice];
+            }
+
+            for (int indice = 0; indice < Math.Sqrt(matriz.Length); indice++)
+            {
+                diagonalSecundaria += matriz[indice, (int)(Math.Sqrt(matriz.Length) - 1 - indice)] + " ";
+                somaSecundaria += matriz[indice, (int)(Math.Sqrt(matriz.Length) - 1 - indice)];
+            }
+
+            Console.WriteLine("Elementos da Diagonal Principal " + diagonalPrincipal);
+            Console.WriteLine("Soma da Diagonal Principal " + somaPrincipal);
+            Console.WriteLine("Elementos da Diagonal Secundária " + diagonalSecundaria); 
+            Console.WriteLine("Soma da Diagonal Secundária " + somaSecundaria);
+        }
+    }
+}
---
+++ debug/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace debug;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] vetor01 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        for (int indice = 0; indice < 10; indice++)
+        {
+            Console.WriteLine($"vetor01[{indice}] = {vetor01[indice]}");
+        }
+    }
+}
