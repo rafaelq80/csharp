041e6a30e12bb280080b3a47a18452d03111006d
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-09-04 08:29:10 -0300

    Exemplo - Interface

--- aula_10/Funcionarios/Program.cs
+++ aula_10/Funcionarios/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Gerente g1 = new Gerente(6000.00F, "Gerente", "Pedro", 0.10F);
+            Console.WriteLine($"Calcular Salário do Gerente: {g1.CalcularSalario()}");
+
+            Vendedor v1 = new Vendedor(2500, "Vendedor", "Pedro", 10000.00F);
+            Console.WriteLine($"Calcular Salário do Vendedor: {v1.CalcularSalario()}");
+
+            OperadorCaixa op1 = new OperadorCaixa(2500.00F, "Operador de Caixa", "Pedro");
+            Console.WriteLine($"Calcular Salário do Operador de Caixa: {op1.CalcularSalario()}");
         }
     }
 }
